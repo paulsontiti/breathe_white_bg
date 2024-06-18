785b51ce179676f4694d0adda8a7cedfa5d045a7
paulson_nwoha <paulsontiti@gmail.com>
2024-06-18 09:01:47 +0100

    call to action buttons removed from product component

--- src/app/components/product.tsx
+++ src/app/components/product.tsx
@@ -12,7 +12,7 @@ function Product({ title, description, imgSrc }: Props) {
       <h3 className="text-3xl my-4">{title}</h3>
       <img src={imgSrc} alt={title} className="my-4 h-72" />
       <p className="my-4 px-8">{description}</p>
-      <CallToAction />
+     
     </div>
   );
 }
